0548933e4f3f53ffbfeec5aeaf49d95771b5cd53
jorden95 <michiel.jordens@imec.be>
2022-08-01 11:23:26 +0200

    Added trapezoid rise time and flat top

--- src/pages/rbs_detectors_overview.js
+++ src/pages/rbs_detectors_overview.js
@@ -1,24 +1,31 @@
 import React, {useContext} from "react";
-import {HiveConfig} from "../App";
+import {HiveConfig, HiveUrl} from "../App";
 import {TableHeader, TableRow} from "../components/table_elements";
+import {useData} from "../components/generic_control";
 
 export function RbsDetectorOverview() {
     const hiveConfig = useContext(HiveConfig);
     let detectors = hiveConfig?.rbs?.hardware?.caen?.detectors;
     let table = [];
 
-    for (let detector of detectors) {
-        let binning = "(" + detector.bins_min + ":" + detector.bins_max + ") -> " + detector.bins_width
+    const hiveUrl = useContext(HiveUrl)
 
-        table.push(<TableRow key={detector.identifier} items={[ detector.identifier, detector.board, detector.channel,
-            binning
+    const [data,,] = useData(hiveUrl + hiveConfig?.rbs?.hardware?.caen?.proxy);
 
-
-        ]}/>)
-
-
+    let boards = {}
+    if (data.boards) {
+        for (let board of data?.boards) {
+            boards[board.id] = board
+        }
     }
 
+    for (let detector of detectors) {
+        let active_channel = boards[detector.board]?.channels[detector.channel]
+        let binning = "(" + detector.bins_min + ":" + detector.bins_max + ") -> " + detector.bins_width
+        table.push(<TableRow key={detector.identifier} items={[ detector.identifier, detector.board, detector.channel,
+            binning, active_channel?.trapezoid_rise_time + " ns", active_channel?.trapezoid_flat_top + " ns"
+        ]}/>)
+    }
 
     return (
         <div>
@@ -26,7 +33,7 @@ export function RbsDetectorOverview() {
             <table className="table table-striped table-hover table-sm">
 
                 <TableHeader
-                    items={["Identifier", "Board", "Channel", "Binning"]}/>
+                    items={["Identifier", "Board", "Channel", "Binning", "Trapezoid Rise Time", "Trapezoid Flat Top"]}/>
                 <tbody>
                 {table}
                 </tbody>
